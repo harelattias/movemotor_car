1dca5afdfcfc39ef18e0f803bf90d18f98f8274d
ד"ר מיאו <109675030+harelattias@users.noreply.github.com>
2022-11-16 17:58:23 +0200

    bias calibration

--- main.ts
+++ main.ts
@@ -1,16 +1,27 @@
+input.onButtonPressed(Button.A, function () {
+    Kitronik_Move_Motor.motorBalance(Kitronik_Move_Motor.SpinDirections.Right, left_bias)
+    left_bias += 1
+    basic.showNumber(left_bias)
+})
+input.onButtonPressed(Button.B, function () {
+    Kitronik_Move_Motor.motorBalance(Kitronik_Move_Motor.SpinDirections.Right, left_bias)
+    left_bias += -1
+    basic.showNumber(left_bias)
+})
 radio.onReceivedValue(function (name, value) {
     if (name.compare("drive") == 0) {
         if (value == 0) {
             Kitronik_Move_Motor.stop()
         }
         if (value == 1) {
-            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 0)
+            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 50)
         }
         if (value == -1) {
-            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, 0)
+            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Reverse, 50)
         }
     }
 })
+let left_bias = 0
 radio.setGroup(111)
 basic.forever(function () {
 	
