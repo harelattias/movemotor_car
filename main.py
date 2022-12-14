271c302ca11b402c7dcca437b8241fe3da94e437
ד"ר מיאו <109675030+harelattias@users.noreply.github.com>
2022-12-14 16:49:09 +0200

    👈👉✔

--- main.py
+++ main.py
@@ -1,96 +1,100 @@
-def FOREVER():
-    global speed_forward
-    speed_forward = last_command_speed_fwd
-    if speed_forward > 30:
-        filter_distance()
-        if filtered_distance < 20:
-            Kitronik_Move_Motor.stop()
-            stop_blink(1500)
-            normal_lights()
-        else:
-            Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_LEFT,
-                Kitronik_Move_Motor.MotorDirection.FORWARD,
-                speed_forward)
-            Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_RIGHT,
-                Kitronik_Move_Motor.MotorDirection.FORWARD,
-                speed_forward)
-    elif speed_forward < -30:
-        Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_LEFT,
-            Kitronik_Move_Motor.MotorDirection.REVERSE,
-            49)
-        Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_RIGHT,
-            Kitronik_Move_Motor.MotorDirection.REVERSE,
-            49)
-    else:
-        Kitronik_Move_Motor.stop()
-def normal_lights():
-    global moveMotorZIP
+function set_motor_speed (speed: number, motor: boolean) {
+    if (speed > 0) {
+        if (motor) {
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, speed)
+        } else {
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, speed)
+        }
+    } else {
+        if (!(motor)) {
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, speed)
+        } else {
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, speed)
+        }
+    }
+}
+function normal_lights () {
     moveMotorZIP.clear()
-    moveMotorZIP = Kitronik_Move_Motor.create_move_motor_zipled(4)
-    moveMotorZIP.set_zip_led_color(0,
-        Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.WHITE))
-    moveMotorZIP.set_zip_led_color(1,
-        Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.WHITE))
-    moveMotorZIP.set_zip_led_color(2,
-        Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.ORANGE))
-    moveMotorZIP.set_zip_led_color(3,
-        Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.ORANGE))
+    moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
+    moveMotorZIP.setZipLedColor(0, Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.White))
+    moveMotorZIP.setZipLedColor(1, Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.White))
+    moveMotorZIP.setZipLedColor(2, Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Orange))
+    moveMotorZIP.setZipLedColor(3, Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Orange))
     moveMotorZIP.show()
-def stop_blink(time_in_ms: number):
+}
+function stop_blink (time_in_ms: number) {
     moveMotorZIP.clear()
-    moveMotorZIP.set_zip_led_color(0,
-        Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.WHITE))
-    moveMotorZIP.set_zip_led_color(1,
-        Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.WHITE))
-    moveMotorZIP.set_zip_led_color(2,
-        Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.RED))
-    moveMotorZIP.set_zip_led_color(3,
-        Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.RED))
-    for index in range(time_in_ms / blink_time):
+    moveMotorZIP.setZipLedColor(0, Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.White))
+    moveMotorZIP.setZipLedColor(1, Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.White))
+    moveMotorZIP.setZipLedColor(2, Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red))
+    moveMotorZIP.setZipLedColor(3, Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red))
+    for (let index = 0; index < time_in_ms / blink_time; index++) {
         moveMotorZIP.rotate(2)
-        music.play_tone(659, music.beat(BeatFraction.WHOLE))
+        music.playTone(659, music.beat(BeatFraction.Whole))
         basic.pause(blink_time)
         moveMotorZIP.show()
-def ORR(text: str, num: number):
-    global last_command_speed_fwd, last_command_speed_right
-    if text.compare("cmd_fwd") == 0:
+    }
+}
+function ORR (text: string, num: number) {
+	
+}
+radio.onReceivedString(function (receivedString) {
+    if (receivedString.compare("beep") == 0) {
+        Kitronik_Move_Motor.beepHorn()
+    }
+    if (receivedString.compare("beeeeeeeep") == 0) {
+        music.playMelody("C5 C5 C5 C5 C5 C5 C5 C5 ", 120)
+    }
+})
+radio.onReceivedValue(function (name, value) {
+    if (text.compare("cmd_fwd") == 0) {
         last_command_speed_fwd = num
-    if text.compare("cmd_rgt") == 0:
+    }
+    if (text.compare("cmd_rgt") == 0) {
         last_command_speed_right = num
-
-def on_received_string(receivedString):
-    if receivedString.compare("beep") == 0:
-        Kitronik_Move_Motor.beep_horn()
-    if receivedString.compare("beeeeeeeep") == 0:
-        music.play_melody("C5 C5 C5 C5 C5 C5 C5 C5 ", 120)
-radio.on_received_string(on_received_string)
-
-def on_received_value(name, value):
-    ORR(name, value)
-radio.on_received_value(on_received_value)
-
-def filter_distance():
-    global filtered_distance
-    Kitronik_Move_Motor.set_ultrasonic_units(Kitronik_Move_Motor.Units.CENTIMETERS)
-    if Kitronik_Move_Motor.measure() != 0 and Kitronik_Move_Motor.measure() < 200:
+    }
+})
+function filter_distance () {
+    Kitronik_Move_Motor.setUltrasonicUnits(Kitronik_Move_Motor.Units.Centimeters)
+    if (Kitronik_Move_Motor.measure() != 0 && Kitronik_Move_Motor.measure() < 200) {
         filtered_distance = Kitronik_Move_Motor.measure()
-    radio.send_value("filterd distance", filtered_distance)
-    radio.send_value("measured distance", Kitronik_Move_Motor.measure())
-last_command_speed_right = 0
-speed_forward = 0
-last_command_speed_fwd = 0
-blink_time = 0
-filtered_distance = 0
-moveMotorZIP: Kitronik_Move_Motor.MoveMotorZIP = None
-moveMotorZIP = Kitronik_Move_Motor.create_move_motor_zipled(4)
+    }
+    radio.sendValue("filterd distance", filtered_distance)
+    radio.sendValue("measured distance", Kitronik_Move_Motor.measure())
+}
+let speed_left_motor = 0
+let speed_right_motor = 0
+let speed_right = 0
+let last_command_speed_right = 0
+let last_command_speed_fwd = 0
+let blink_time = 0
+let filtered_distance = 0
+let moveMotorZIP: Kitronik_Move_Motor.MoveMotorZIP = null
+moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
 normal_lights()
-radio.set_group(111)
-Kitronik_Move_Motor.motor_balance(Kitronik_Move_Motor.SpinDirections.RIGHT, 1)
+radio.setGroup(111)
+Kitronik_Move_Motor.motorBalance(Kitronik_Move_Motor.SpinDirections.Right, 1)
 filtered_distance = 100
 blink_time = 250
 last_command_speed_fwd = 0
-speed_forward = 0
-
-def on_forever():
-    FOREVER()
-basic.forever(on_forever)
+let speed_forward = 0
+basic.forever(function () {
+    speed_forward = last_command_speed_fwd
+    speed_right = last_command_speed_right
+    if (speed_forward > -30 && speed_forward < 30) {
+        speed_forward = 0
+    }
+    if (speed_right > -50 && speed_right < 50) {
+        speed_right = 0
+    }
+    speed_right_motor = speed_forward - speed_right / 2
+    speed_left_motor = speed_forward + speed_right / 2
+    filter_distance()
+    if (filtered_distance < 20 && speed_forward > 0) {
+        Kitronik_Move_Motor.stop()
+        stop_blink(500)
+        normal_lights()
+    } else {
+        set_motor_speed(speed_forward, true)
+    }
+})
--- main.ts
+++ main.ts
@@ -1,23 +1,20 @@
-function FOREVER () {
-    speed_forward = last_command_speed_fwd
-    speed_right = last_command_speed_right
-    speed_right_motor = speed_forward - speed_right
-    speed_left_motor = speed_forward + speed_right
-    filter_distance()
-    if (speed_forward > 30) {
-        if (filtered_distance < 20) {
-            Kitronik_Move_Motor.stop()
-            stop_blink(1500)
-            normal_lights()
+function set_motor_speed (speed: number, motor: boolean) {
+    if (motor) {
+        if (speed > 0) {
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, speed)
+        } else if (speed < 0) {
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Reverse, speed)
         } else {
-            set_right_motor_speed(speed_forward)
-            set_left_motor_speed2(speed_forward)
+            Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
         }
-    } else if (speed_forward < -30) {
-        set_right_motor_speed(speed_forward)
-        set_left_motor_speed2(speed_forward)
     } else {
-        Kitronik_Move_Motor.stop()
+        if (speed > 0) {
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, speed)
+        } else if (speed < 0) {
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Reverse, speed)
+        } else {
+            Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorLeft)
+        }
     }
 }
 function normal_lights () {
@@ -42,14 +39,6 @@ function stop_blink (time_in_ms: number) {
         moveMotorZIP.show()
     }
 }
-function ORR (text: string, num: number) {
-    if (text.compare("cmd_fwd") == 0) {
-        last_command_speed_fwd = num
-    }
-    if (text.compare("cmd_rgt") == 0) {
-        last_command_speed_right = num
-    }
-}
 radio.onReceivedString(function (receivedString) {
     if (receivedString.compare("beep") == 0) {
         Kitronik_Move_Motor.beepHorn()
@@ -58,48 +47,51 @@ radio.onReceivedString(function (receivedString) {
         music.playMelody("C5 C5 C5 C5 C5 C5 C5 C5 ", 120)
     }
 })
-function set_left_motor_speed2 (speed: number) {
-    if (speed > 0) {
-        Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, speed)
-    } else {
-        Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Reverse, speed * -1)
-    }
-}
 radio.onReceivedValue(function (name, value) {
-    ORR(name, value)
+    if (name.compare("cmd_fwd") == 0) {
+        last_command_speed_fwd = value
+    }
+    if (name.compare("cmd_rgt") == 0) {
+        last_command_speed_right = value
+    }
 })
-function filter_distance () {
-    Kitronik_Move_Motor.setUltrasonicUnits(Kitronik_Move_Motor.Units.Centimeters)
-    if (Kitronik_Move_Motor.measure() != 0 && Kitronik_Move_Motor.measure() < 200) {
-        filtered_distance = Kitronik_Move_Motor.measure()
-    }
-    radio.sendValue("filterd distance", filtered_distance)
-    radio.sendValue("measured distance", Kitronik_Move_Motor.measure())
-}
-function set_right_motor_speed (speed: number) {
-    if (speed > 0) {
-        Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, speed)
-    } else {
-        Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Reverse, speed * -1)
-    }
-}
 let speed_left_motor = 0
 let speed_right_motor = 0
-let last_command_speed_right = 0
 let speed_right = 0
-let speed_forward = 0
+let last_command_speed_right = 0
 let last_command_speed_fwd = 0
 let blink_time = 0
-let filtered_distance = 0
 let moveMotorZIP: Kitronik_Move_Motor.MoveMotorZIP = null
 moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
 normal_lights()
 radio.setGroup(111)
 Kitronik_Move_Motor.motorBalance(Kitronik_Move_Motor.SpinDirections.Right, 1)
-filtered_distance = 100
+let filtered_distance = 100
 blink_time = 250
 last_command_speed_fwd = 0
-speed_forward = 0
+let speed_forward = 0
 basic.forever(function () {
-    FOREVER()
+    speed_forward = last_command_speed_fwd
+    speed_right = last_command_speed_right
+    if (speed_forward > -30 && speed_forward < 30) {
+        speed_forward = 0
+    }
+    if (speed_right > -50 && speed_right < 50) {
+        speed_right = 0
+    }
+    speed_right_motor = speed_forward - speed_right / 10
+    speed_left_motor = speed_forward + speed_right / 10
+    Kitronik_Move_Motor.setUltrasonicUnits(Kitronik_Move_Motor.Units.Centimeters)
+    if (Kitronik_Move_Motor.measure() != 0 && Kitronik_Move_Motor.measure() < 200) {
+        filtered_distance = Kitronik_Move_Motor.measure()
+    }
+    radio.sendValue("filterd distance", filtered_distance)
+    if (filtered_distance < 20 && speed_forward > 0) {
+        Kitronik_Move_Motor.stop()
+        stop_blink(500)
+        normal_lights()
+    } else {
+        set_motor_speed(speed_right_motor, true)
+        set_motor_speed(speed_left_motor, false)
+    }
 })
