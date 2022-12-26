255e054f009eb9c7634cfb3353b97b6603ce762f
ד"ר מיאו <109675030+harelattias@users.noreply.github.com>
2022-12-26 11:02:28 +0200

    Update main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ function set_motor_speed (speed: number, motor: boolean) {
         if (speed > 0) {
             Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, speed)
         } else if (speed < 0) {
-            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Reverse, speed)
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Reverse, Math.abs(speed))
         } else {
             Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
         }
@@ -11,7 +11,7 @@ function set_motor_speed (speed: number, motor: boolean) {
         if (speed > 0) {
             Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, speed)
         } else if (speed < 0) {
-            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Reverse, speed)
+            Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Reverse, Math.abs(speed))
         } else {
             Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorLeft)
         }
@@ -48,18 +48,17 @@ radio.onReceivedString(function (receivedString) {
     }
 })
 radio.onReceivedValue(function (name, value) {
-    if (name.compare("cmd_fwd") == 0) {
-        last_command_speed_fwd = value
+    if (name.compare("rgt_spd") == 0) {
+        last_command_right_motor_speed = value
     }
-    if (name.compare("cmd_rgt") == 0) {
-        last_command_speed_right = value
+    if (name.compare("left_spd") == 0) {
+        last_command_left_motor_speed = value
     }
 })
 let speed_left_motor = 0
 let speed_right_motor = 0
-let speed_right = 0
-let last_command_speed_right = 0
-let last_command_speed_fwd = 0
+let last_command_left_motor_speed = 0
+let last_command_right_motor_speed = 0
 let blink_time = 0
 let moveMotorZIP: Kitronik_Move_Motor.MoveMotorZIP = null
 moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
@@ -68,25 +67,16 @@ radio.setGroup(111)
 Kitronik_Move_Motor.motorBalance(Kitronik_Move_Motor.SpinDirections.Right, 1)
 let filtered_distance = 100
 blink_time = 250
-last_command_speed_fwd = 0
-let speed_forward = 0
+last_command_right_motor_speed = 0
 basic.forever(function () {
-    speed_forward = last_command_speed_fwd
-    speed_right = last_command_speed_right
-    if (speed_forward > -30 && speed_forward < 30) {
-        speed_forward = 0
-    }
-    if (speed_right > -50 && speed_right < 50) {
-        speed_right = 0
-    }
-    speed_right_motor = speed_forward - speed_right / 10
-    speed_left_motor = speed_forward + speed_right / 10
+    speed_right_motor = last_command_right_motor_speed
+    speed_left_motor = last_command_left_motor_speed
     Kitronik_Move_Motor.setUltrasonicUnits(Kitronik_Move_Motor.Units.Centimeters)
     if (Kitronik_Move_Motor.measure() != 0 && Kitronik_Move_Motor.measure() < 200) {
         filtered_distance = Kitronik_Move_Motor.measure()
     }
     radio.sendValue("filterd distance", filtered_distance)
-    if (filtered_distance < 20 && speed_forward > 0) {
+    if (filtered_distance < 20 && speed_left_motor + speed_right_motor > 0) {
         Kitronik_Move_Motor.stop()
         stop_blink(500)
         normal_lights()
