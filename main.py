0f80224435fb0f4b0f422ebe4238f3797068774c
ד"ר מיאו <109675030+harelattias@users.noreply.github.com>
2023-02-01 17:05:16 +0200

    v1.4 - enable/disable distance sensor

--- main.py
+++ main.py
@@ -10,17 +10,16 @@ def set_motor_speed(speed: number, motor: bool):
                 abs(speed))
         else:
             Kitronik_Move_Motor.motor_off(Kitronik_Move_Motor.Motors.MOTOR_RIGHT)
+    elif speed > 0:
+        Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_LEFT,
+            Kitronik_Move_Motor.MotorDirection.FORWARD,
+            speed)
+    elif speed < 0:
+        Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_LEFT,
+            Kitronik_Move_Motor.MotorDirection.REVERSE,
+            abs(speed))
     else:
-        if speed > 0:
-            Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_LEFT,
-                Kitronik_Move_Motor.MotorDirection.FORWARD,
-                speed)
-        elif speed < 0:
-            Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_LEFT,
-                Kitronik_Move_Motor.MotorDirection.REVERSE,
-                abs(speed))
-        else:
-            Kitronik_Move_Motor.motor_off(Kitronik_Move_Motor.Motors.MOTOR_LEFT)
+        Kitronik_Move_Motor.motor_off(Kitronik_Move_Motor.Motors.MOTOR_LEFT)
 def normal_lights():
     global moveMotorZIP
     moveMotorZIP.clear()
@@ -51,6 +50,11 @@ def stop_blink(time_in_ms: number):
         moveMotorZIP.show()
 
 def on_received_string(receivedString):
+    global stop_by_dist
+    if receivedString.compare("on_dis") == 0:
+        stop_by_dist = 1
+    if receivedString.compare("off_dis") == 0:
+        stop_by_dist = 0
     if receivedString.compare("off_lig") == 0:
         moveMotorZIP.set_color(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.BLACK))
         moveMotorZIP.show()
@@ -71,6 +75,7 @@ radio.on_received_value(on_received_value)
 speed_left_motor = 0
 speed_right_motor = 0
 last_command_left_motor_speed = 0
+stop_by_dist = 0
 last_command_right_motor_speed = 0
 blink_time = 0
 moveMotorZIP: Kitronik_Move_Motor.MoveMotorZIP = None
@@ -90,7 +95,7 @@ def on_forever():
     if Kitronik_Move_Motor.measure() != 0 and Kitronik_Move_Motor.measure() < 200:
         filtered_distance = Kitronik_Move_Motor.measure()
     radio.send_value("filterd distance", filtered_distance)
-    if filtered_distance < 20 and speed_left_motor + speed_right_motor > 0:
+    if filtered_distance < 5 and speed_left_motor + speed_right_motor > 0 and stop_by_dist == 1:
         Kitronik_Move_Motor.stop()
         stop_blink(500)
         normal_lights()
